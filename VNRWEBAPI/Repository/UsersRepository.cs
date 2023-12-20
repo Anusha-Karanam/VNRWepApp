9f8ff47c425095f54214aad1a09613ee6707a973
EI13153 <EI13153@ELW5234>
2023-12-20 15:24:27 +0530

    Add project files.

---
+++ VNRWEBAPI/Repository/UsersRepository.cs
@@ -0,0 +1,49 @@
+﻿using VNRWEBAPI.Models;
+using VNRWEBAPI.ViewModels;
+
+namespace VNRWEBAPI.Repository
+{
+    public class UsersRepository: IUsersRepository
+    {
+        
+        
+		private readonly newDbClgContext _context;//declaring private variable
+        public UsersRepository(newDbClgContext dbContext)//Constructor 
+        {
+            _context = dbContext;
+        }
+        public List<string>? Login(LoginVM loginVm)
+        {
+            string hashedPassword = HashingHelper.HashPassword(loginVm.Password);//Password hashing
+            var user = _context.Users.FirstOrDefault(u => u.Username == loginVm.UserName && u.Password== hashedPassword);//Comparing login credential
+            /*Retrieving the Name of particular user*/
+            List<string> LoginValue = new List<string>();
+            if (user != null)
+                {
+                    var adminName = "Admin";
+                    var teacherName = "";
+                    var studentName = "";
+                    teacherName =
+                    (from teacher in _context.Teachers
+                     where teacher.UserId == user.Id
+                     select teacher.Name
+                    ).FirstOrDefault();
+                    studentName = (
+                    from student in _context.Students
+                    where student.UserId == user.Id
+                    select student.Name
+                    ).FirstOrDefault();
+
+                    var NameOfUser = teacherName ?? studentName ?? adminName;
+                    {
+                        LoginValue.Add(NameOfUser);
+                        LoginValue.Add(user.Username);
+                        LoginValue.Add(user.UserType.ToString());
+                        LoginValue.Add(user.Id.ToString());
+                    }
+                return LoginValue;
+            }
+            return null;
+        }   
+    }
+}
